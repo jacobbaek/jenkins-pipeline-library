7934a847a39c5ff987ef6de694375494dfae14c2
jacobbaek <jacobbaek@osci.kr>
2020-09-22 14:55:19 +0900

    modified getopenstackvminfo.groovy

--- vars/getOpenstackVMinfo.groovy
+++ vars/getOpenstackVMinfo.groovy
@@ -4,7 +4,7 @@ def call(String namePrefix, String, networkname, String provider) {
   println("Start getting Openstack Network informations...")
   fetchCloudsConf()
   ipaddr = sh(returnStdout: true,
-        script: "openstack server show ${namePrefix} --os-cloud ${provider} -f json | jq '.addresses'").trim()
+        script: "openstack server show ${namePrefix} --os-cloud ${provider} -f json | jq '.addresses' | awk -F '[;=]' 'for (i=1; i<=NF; i++) print \$i'").trim()
   println("Getting Openstack VM's IP Addresses has been finished!")
   return ipaddr
 }
