b7a2f59cfd27338b7eb9dcc8e8ccdd6b22f45170
jacobbaek <jacobbaek@osci.kr>
2020-09-22 15:11:51 +0900

    Modified argument and add using grep command

--- vars/getOpenstackVMinfo.groovy
+++ vars/getOpenstackVMinfo.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 g
-def call(String namePrefix, String, networkname, String provider) {
+def call(String namePrefix, String networkName, String provider) {
   println("Start getting Openstack Network informations...")
   fetchCloudsConf()
   ipaddr = sh(returnStdout: true,
-        script: "openstack server show ${namePrefix} --os-cloud ${provider} -f json | jq '.addresses' | awk -F '[;=]' 'for (i=1; i<=NF; i++) print \$i'").trim()
+        script: "openstack server show ${namePrefix} --os-cloud ${provider} -f json | jq '.addresses' | awk -F '[;=]' 'for (i=1; i<=NF; i++) print \$i' | grep -A1 $networkName").trim()
   println("Getting Openstack VM's IP Addresses has been finished!")
   return ipaddr
 }
