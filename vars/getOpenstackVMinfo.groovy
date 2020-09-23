dcc7e61d4e0add0e811b2b887e447f02ff9d70cc
jacobbaek <jacobbaek@osci.kr>
2020-09-23 11:10:39 +0900

    Update redefined getopenstackvminfo.groovy

--- vars/getOpenstackVMinfo.groovy
+++ vars/getOpenstackVMinfo.groovy
@@ -1,10 +1,17 @@
 #!/usr/bin/env groovy
-
-def call(String namePrefix, String networkName, String provider) {
-  println("Start getting Openstack Network informations...")
+def call(String namePrefix, String networkType="mgmt", String provider='taco-prod') {
   fetchCloudsConf()
-  ipaddr = sh(returnStdout: true,
-    script: "openstack server show ${namePrefix} --os-cloud ${provider} -f json | jq '.addresses' | awk -F '[;=]' '{for (i=1; i<=NF; i++) print \$i}' | grep -A1 $networkName | tail -n -1").trim()
-  println("Getting Openstack VM's IP Address(${ipaddr}) has been finished!")
-  return ipaddr
+
+	def result = [:]
+	infos = sh(returnStdout: true, script: "openstack server list --os-cloud ${provider} -f json | jq --raw-output 'sort_by(.Name) | .[] | select(.Name|test(\"${namePrefix}\")) | .Name, .Networks'").split('\n')
+	print infos
+	for (i = 0; i < infos.size(); i=i+2) {
+	  infos[i+1].split(";").each { it ->
+      if(it.contains(networkType)) {
+				ip = it.split("=")[-1]
+	  		result[infos[i]] = ip
+			}
+		}
+	}
+	return result
 }
